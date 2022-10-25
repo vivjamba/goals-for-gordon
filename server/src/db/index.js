9e55be3bf1add7e9e42154c69cd7eed3fd473946
jkvin114 <iu889@naver.com>
2022-10-25 14:13:29 -0400

    added goal and comment generator functions

--- server/src/db/index.js
+++ server/src/db/index.js
@@ -18,6 +18,9 @@ db.on("error", function () {
 //on connection success
 db.once("open", function () {
     console.log("Connected to mongodb!")
+    // testCreate()
+    generateGoals("635823b166a32968b45e328c")
+    generateComments("635823b166a32968b45e328c","635825a2a4f26f9aa1d78f3e")
 })
 
 const User = mongoose.model('User', userSchema);
@@ -43,15 +46,66 @@ const Goal = mongoose.model('Goal', goalSchema);
 //plz stop executing this function. There are already 1000 employees created .
 async function testCreate() {
 	//create document from JSON
-	for(let i=1;i<10;++i){
+	for(let i=1;i<100;++i){
 		//upload first 10 data from json file
 		const resolvedData = await User.create(employees[i])
 	}
+
 }
+function randNumber(){
+    return Math.floor(Math.random()*new Date().valueOf())
+}
+
+/**
+ * generate goal documents
+ * @param {*} posterId id of poster as string
+ */
+async function generateGoals(posterId) {
+	//create document from JSON
+    let date=new Date()
+    let count=2
+	for(let i=0;i<count;++i){
+		//upload first 10 data from json file
+		const resolvedData = await Goal.create({
+            title:"goal_"+randNumber(),
+            description:"an auto-generated goal",
+            startDate:date,
+            endDate:date,
+            category:"none",
+            status:false,
+            poster:mongoose.Types.ObjectId(posterId) 
+        })
+        console.log(resolvedData)
+	}
+    
+
+}
+
+/**
+ * generate comment documents
+ * @param {*} posterId id of poster as string
+ * @param {*} goalId id of goal as string
+ */
+async function generateComments(posterId,goalId) {
+	//create document from JSON
+    let count=2
+	for(let i=0;i<count;++i){
+		//upload first 10 data from json file
+		const resolvedData = await Comment.create({
+            content:"an auto-generated comment"+randNumber(),
+            poster:mongoose.Types.ObjectId(posterId) ,
+            goal:mongoose.Types.ObjectId(goalId) 
+        })
+        console.log(resolvedData)
+	}
+    
+
+}
+
 // Just a funky test function
 async function testFetch() {
 	//fetch all documents
-	return await User.findByEmail("Iva_Page@fluffybunnyconsulting.com")
+	return await User.findAll()
 }
 // testCreate();
 module.exports = {
