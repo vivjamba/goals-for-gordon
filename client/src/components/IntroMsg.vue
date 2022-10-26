75481f2ee2aaed3cc64701b90299b465c04f162f
Harrison Coleman <hkcoleman7901@gmail.com>
2022-10-26 17:50:31 -0400

    log-in page up

---
+++ client/src/components/IntroMsg.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="greetings">
+        <h1 class="green">{{msg}}</h1>
+        <h3>Employee Task Manager</h3>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "IntroMsg",
+    props: {
+        msg: String,
+    }
+}
+</script>
+
+<style scoped>
+h1 {
+  font-weight: 500;
+  font-size: 2.6rem;
+  top: -10px;
+}
+
+h3 {
+  font-size: 1.2rem;
+}
+
+.greetings h1,
+.greetings h3 {
+  text-align: center;
+}
+
+@media (min-width: 1024px) {
+  .greetings h1,
+  .greetings h3 {
+    text-align: left;
+  }
+}
+</style>
---
+++ client/src/components/LogIn.vue
@@ -0,0 +1,79 @@
+<template>
+    <div class="center">
+        <h1>Sign In</h1>
+        <form action="">
+            <br>
+            <input type="text" id="username" name="username" placeholder="Username">
+            <br>
+            <input type="password" id="pwd" name="pwd" placeholder="Password">
+            <br>
+            <input type="submit" value="Log-In">
+        </form>
+    </div>
+    
+</template>
+
+<script>
+export default {
+    name: "LogIn"
+}
+</script>
+
+<style scoped>
+.center {
+text-align: center;
+color: white;
+border-radius: 4px;
+background-color: #005151;
+padding: 40px;
+box-shadow: 0px 5px 18px 5px #AAAAAA;
+}
+
+input[type="text"],
+input[type="password"]{
+    width: 327.67px;
+    height: 59.24px;
+    left: 557.21px;
+    top: 738.25px;
+
+    background: #FFFFFF;
+    border-radius: 4px;
+
+    
+    font-size: 20px;
+    line-height: 20px;
+    text-align: center;
+    border: none;
+    margin: 10px;
+}
+::placeholder{
+    opacity: .75;
+}
+
+input:focus::placeholder {
+  color: transparent;
+}
+
+input[type="submit"]{
+    width: 327.67px;
+    height: 59.24px;
+    left: 557.21px;
+    top: 738.25px;
+
+    background: #66CBBD;
+    border-radius: 4px;
+
+    font-size: 20px;
+    line-height: 20px;
+    text-align: center;
+    text-transform: capitalize;
+    color: #FFFFFF;
+    border: none;
+    margin: 10px;
+    margin-top: 40px;
+}
+input[type="submit"]:hover{
+    background: #59b9ac;
+}
+
+</style>
