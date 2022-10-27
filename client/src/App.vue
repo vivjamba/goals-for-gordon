d9d5de68e7b335a72efad919b2659d3622e59a1b
Chris Evans <cwevans612@gmail.com>
2022-10-27 14:46:56 -0400

    ACtivated router

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,24 +1,12 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
-import IntroMsg from './components/IntroMsg.vue'
 import LogIn from './components/LogIn.vue'
+import HomeView from './views/HomeView.vue'
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/ukg.svg" width="250" height="250" />
 
-    <div class="msg">
-      <IntroMsg msg="Goals for Gordon" />
-
-      <!-- <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav> -->
-    </div>
-  </header>
-
-  <LogIn/>
+    <RouterView></RouterView>
 </template>
 
 <style scoped>
--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -1,8 +1,17 @@
 <script setup lang="ts">
-//import TheWelcome from '../components/TheWelcome.vue'
 import LogIn from '../components/LogIn.vue'
+import IntroMsg from '../components/IntroMsg.vue'
+
 </script>
 
 <template>
-  <LogIn/>
+    <header>
+        <img alt="Vue logo" class="logo" src="@/assets/ukg.svg" width="250" height="250" />
+
+        <div class="msg">
+          <IntroMsg msg="Goals for Gordon" />
+        </div>
+    </header>
+
+    <LogIn/>
 </template>
