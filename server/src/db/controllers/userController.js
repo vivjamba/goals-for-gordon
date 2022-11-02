9db33166251139bdcb3d9a0fc0a37dc572257f94
SiCh3 <sichircu@gmail.com>
2022-11-01 20:47:37 -0400

    changed controller names, fixed some controller logic issues

--- server/src/db/controllers/userController.js
+++ server/src/db/controllers/userController.js
@@ -1,26 +1,44 @@
 const { User } = require("../index")
 
-exports.user_list_all = (req, res) => {
+//find all users
+exports.list_all_users = (req, res) => {
     User.find({}).then((data) => res.send(data));
 }
-exports.user_find_email = (req, res) => {
+
+//find users by email
+exports.find_by_email = (req, res) => {
     User.find({ email: req.params.email }).then((data) => res.send(data))
 }
-exports.user_find_id = (req, res) => {
-    User.find({ employeeId: req.params.employeeId }).then((data) => res.send(data))
-}
-exports.user_manager_find_employees = (req, res) => {
-    User.find({
-        managerId: req.params.managerId,
-        companyId: req.params.companyId,
-        isManager: false
+
+//find user by ID (employee + company)
+exports.find_user_by_id = (req, res) => {
+    User.find({ 
+        employeeId: req.params.employeeId, 
+        companyId: req.params.companyId
     }).then((data) => res.send(data))
 }
-exports.user_employee_find_manager = (req, res) => {
+
+//find user by Mongoose ID
+exports.find_user_by_mongo_id = (req, res) => {
+    User.find({ 
+        _id: req.params.mongo_id, 
+    }).then((data) => res.send(data))
+}
+
+//find an employee's manager 
+exports.find_manager_by_id = (req, res) => {
+    User.find({
+        employeeID: req.params.managerId,
+        companyId: req.params.companyId,
+        isManager: true
+    }).then((data) => res.send(data))
+}
+
+//find all of a manager's employees  
+exports.find_employees_by_manager = (req, res) => {
     User.find({
         managerId: req.params.managerId,
         companyId: req.params.companyId,
-        isManager: true
     }).then((data) => res.send(data))
 }
 
--- server/src/routes/user.js
+++ server/src/routes/user.js
@@ -2,7 +2,6 @@ const express = require('express');
 const bodyParser = require('body-parser')
 const router = express.Router()
 const user_controller = require("../db/controllers/userController.js")
-//const {User}=require("./../db/index")
 
 //parser
 router.use(bodyParser.json())
@@ -10,16 +9,23 @@ router.use(bodyParser.text())
 
 //READ
 
-//sends all users
-router.get("/list", user_controller.user_list_all)
-//sends all users with the email
-router.get("/:email", user_controller.user_find_email)
-//sends user with given employeeId
-router.get("/id/:employeeId", user_controller.user_find_id)
-//sends employees manager
-router.get("/manager/:companyId-:managerId", user_controller.user_employee_find_manager)
-//sends all users who have given manager
-router.get("/manager/list/:companyId-:managerId", user_controller.user_manager_find_employees)
+//get all users
+router.get("/list", user_controller.list_all_users)
+
+//get users by email
+router.get("/:email", user_controller.find_by_email)
+
+//get user identified by employeeId + companyId
+router.get("/id/:companyId-:employeeId", user_controller.find_user_by_id)
+
+//get user identified by Mongoose _id
+router.get("/_id/:mongo_id", user_controller.find_user_by_mongo_id)
+
+//get an employee's manager identified by employeeId + companyId
+router.get("/manager/:companyId-:managerId", user_controller.find_manager_by_id)
+
+//get all of a manager's employees
+router.get("/manager/list/:companyId-:managerId", user_controller.find_employees_by_manager)
 
 //UPDATE
 
