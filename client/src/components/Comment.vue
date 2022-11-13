ba12c5c6dee82a04eae9545606e50b457b3e3fd7
Harrison Coleman <hkcoleman7901@gmail.com>
2022-11-13 15:32:52 -0500

    save

--- client/src/components/Comment.vue
+++ client/src/components/Comment.vue
@@ -1,13 +1,40 @@
 <template>
-    
+    <div class="comment">
+        <h2 class="poster">{{poster}}</h2>
+        <h3 class="content">{{content}}</h3>
+        <h3 class="date">{{date}}</h3>
+    </div>
 </template>
 
 <script>
 export default {
-    name: 'Comment'
+    name: 'Comment',
+    props: {
+        content: String,
+        poster: String,
+        date: String, 
+    }
 }
 </script>
 
 <style scoped>
-
+.comment{
+    border-radius: 5px;
+    padding-bottom: 10px;
+    background-color: #DDDDDD;
+    position: relative;
+    margin: 10px;
+}
+.poster{
+    padding-left: 10px;
+}
+.content{
+    padding-left: 30px;
+}
+.date{
+    position: absolute;
+    top: 2%;
+    right: 2%;
+    
+}
 </style>
--- client/src/components/goals/CommentThread.vue
+++ client/src/components/goals/CommentThread.vue
@@ -1,10 +1,19 @@
 <template>
+    <div>
+        <Comment content="content" date="date" poster="poster"/>
+        <Comment content="content" date="date" poster="poster"/>
+    </div>
     
 </template>
 
 <script>
+
+import Comment from '../Comment.vue'
 export default {
-    name: 'GoalModalComment'
+    name: 'GoalModalComment',
+    components: {
+        Comment,
+    }
 }
 </script>
 
