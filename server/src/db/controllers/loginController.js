ad69b4ada46064f78dab467a8c2a50aacb08dba2
jkvin114 <iu889@naver.com>
2022-11-29 13:49:33 -0500

    Update loginController.js

--- server/src/db/controllers/loginController.js
+++ server/src/db/controllers/loginController.js
@@ -61,7 +61,7 @@ async function loginController(req, res, next){
         const body = { _id: user._id, email: user.email,employeeId:user.employeeId};
         const token = jwt.sign({ user: body },KEY);  //create jwt
 
-          return res.json({ token }); //send signed jwt to client
+          return res.json({ token,id:user._id }); //send signed jwt to client
 
       } catch (error) {
         onServerError(res,err)
